dcfecfbf31798a9f836285d5b74cde96a6ce000e
Shaun Gittins <shaungg97@hotmail.com>
2021-08-13 21:44:58 +1000

    Installed SDL2

--- GameEngine/Main.cpp
+++ GameEngine/Main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <SDL.h>
 
-int main() {
+int main(int argc, char* args[]) {
+	SDL_Init(SDL_INIT_EVERYTHING);
 	std::cout << "Test" << std::endl;
 	std::cin.get();
+	return 0;
 }
